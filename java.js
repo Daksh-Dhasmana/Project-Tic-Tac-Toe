6021ae6c8f5ef15bbaeddece19cdc04dc40cd52f
Daksh-Dhasmana <dakshdhasmana@gmail.com>
2026-04-15 15:02:17 +0530

    finished getting User input and starting to get user input directly on grid

--- java.js
+++ java.js
@@ -1,9 +1,7 @@
-
-// function playerInput(){// A constructor for Player object
-//     let nm=prompt(`Enter your name, Player: `);
-//     let sm=prompt(`Enter your Symbol(X or O), Player: `);
-//     return {nm,sm}
-// }
+const res=document.getElementById("dispRes");
+function playerInput(nm,sm){// A constructor for Player object
+    return {nm,sm}
+}
 const mom = (function () {// A factory Function containing necessery functions
     let matrix;
     function createGameboard(){//Creating a gameboard
@@ -50,41 +48,40 @@ mom.dispBoard();
 //     console.log(row.join(" "));
 // });
 
-// console.log("START THE GAME!!");
+let nm, sm=prompt("enter your Name and Symbol(X or O) Player 1: \n");
+const P1=playerInput(nm,sm);
 
-// console.log("enter your details Player 1: \n")
-// const P1=playerInput();
-
-// console.log("enter your details Player 2: \n")
-// const P2=playerInput();
+let nm2, sm2=prompt("enter your Name and Symbol(X or O) Player 2: \n");
+const P2=playerInput(nm2 , sm2);
 
 let a=0,turn=0;
-// while(a<9){//Game Logic
-//     if(checkMatrix(board)==true){
-//         console.log("player 1 has won");break;
-//     }
-//     if(checkMatrix(board)==false){
-//         console.log("player 2 has won");break;
-//     }
-//     if(turn===0){
-//         console.log("Player 1 turn: ");
-//         let input=prompt("Enter the co-ords");
-//         let [i,j]=input.split(" ");
-//         mom.gameController(P1,i,j);
-//         mom.dispBoard();turn=1;a++;continue;
-//     }
-//     if(turn===1){
-//         console.log("Player 2 turn: ");
-//         let input=prompt("Enter the co-ords");
-//         let [i,j]=input.split(" ");
-//         mom.gameController(P2,i,j);
-//         turn=0;a++;
-//         mom.dispBoard();continue;
-//     }
-//         if(a===8){
-//         console.log("It's a DRAW");break;
-//     }
-// }
+while(a<9){//Game Logic
+    if(checkMatrix(board)==true){
+        console.log("player 1 has won");break;
+    }
+    if(checkMatrix(board)==false){
+        console.log("player 2 has won");break;
+    }
+    if(turn===0){
+        console.log("Player 1 turn: ");
+        let input=prompt("Enter the co-ords");
+        let [i,j]=input.split(" ");
+        mom.gameController(P1,i,j);
+        mom.dispBoard();turn=1;a++;continue;
+    }
+    if(turn===1){
+        console.log("Player 2 turn: ");
+        let input=prompt("Enter the co-ords");
+        let [i,j]=input.split(" ");
+        mom.gameController(P2,i,j);
+        turn=0;a++;
+        mom.dispBoard();continue;
+    }
+        if(a===8){
+        console.log("It's a DRAW");break;
+    }
+}
+
 // board.matrix.forEach(row => {
 //     disp(row.join(" "));
 // });
