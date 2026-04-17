b25b84bd09fd3609f1e902d9e66d0e354d2f025b
Daksh-Dhasmana <dakshdhasmana@gmail.com>
2026-04-18 01:31:07 +0530

    Tic Tac Toe Project Finished

--- java.js
+++ java.js
@@ -1,4 +1,6 @@
 const res=document.getElementById("dispRes");
+const cls=document.getElementById("dispgrid");
+let turn=1;
 function playerInput(nm,sm){// A constructor for Player object
     return {nm,sm}
 }
@@ -23,64 +25,92 @@ const mom = (function () {// A factory Function containing necessery functions
             }
         }  
     }
-    function checkMatrix(){//Used for checking win/draw condition
-        
+    function sameSpot(i,j){
+        if(matrix[i][j]=="X"||matrix[i][j]=="O"){
+            return true;
+        }
+        else{
+           return false;
+        }
     }
-    return {createGameboard, gameController,dispBoard,checkMatrix};
+    function checkMatrix(){// Checks the matrix for winning conditions.
+    for(let i=0;i<=2;i++){
+        if(matrix[i][0]==='X'&& matrix[i][1]==='X'&& matrix[i][2]==='X'){return true;}
+        if(matrix[0][i]==='X'&& matrix[1][i]==='X'&& matrix[2][i]==='X'){return true;}
+        if(matrix[i][0]==='O'&&matrix[i][1]==='O'&& matrix[i][2]==='O'){ return false;}
+        if(matrix[0][i]==='O'&&matrix[1][i]==='O'&&matrix[2][i]==='O'){ return false;}
+    }
+    if(matrix[0][0]==='O'&&matrix[1][1]==='O'&&matrix[2][2]==='O'){return false;}
+    if(matrix[2][0]==='O'&&matrix[1][1]==='O'&&matrix[0][2]==='O'){return false;}
+    if(matrix[0][0]==='X'&&matrix[1][1]==='X'&&matrix[2][2]==='X'){return true;}
+    if(matrix[2][0]==='X'&&matrix[1][1]==='X'&&matrix[0][2]==='X'){return true;}
+}
+    return {createGameboard, gameController,dispBoard,checkMatrix,sameSpot};
 })();
-
-mom.createGameboard();
-mom.dispBoard();
-// function checkMatrix(board){// Checks the matrix for winning conditions.
-//     let t;// X for t=1, O for t=0
-//     for(let i=0;i<=2;i++){
-//         if(board.matrix[i][0]==='X'&&board.matrix[i][1]==='X'&&board.matrix[i][2]==='X'){return true;}
-//         if(board.matrix[0][i]==='X'&&board.matrix[1][i]==='X'&&board.matrix[2][i]==='X'){return true;}
-//         if(board.matrix[i][0]==='O'&&board.matrix[i][1]==='O'&&board.matrix[i][2]==='O'){ return false;}
-//         if(board.matrix[0][i]==='O'&&board.matrix[1][i]==='O'&&board.matrix[2][i]==='O'){ return false;}
-//     }
-//     if(board.matrix[0][0]==='O'&&board.matrix[1][1]==='O'&&board.matrix[2][2]==='O'){return false;}
-//     if(board.matrix[2][0]==='O'&&board.matrix[1][1]==='O'&&board.matrix[0][2]==='O'){return false;}
-//     if(board.matrix[0][0]==='X'&&board.matrix[1][1]==='X'&&board.matrix[2][2]==='X'){return true;}
-//     if(board.matrix[2][0]==='X'&&board.matrix[1][1]==='X'&&board.matrix[0][2]==='X'){return true;}
-// }
-// board.matrix.forEach(row => {
-//     console.log(row.join(" "));
-// });
-
-let nm, sm=prompt("enter your Name and Symbol(X or O) Player 1: \n");
+let nm =prompt("enter your Name Player 1: \n");
+let sm= prompt("enter your Symbol(X or O) Player 1: \n");
 const P1=playerInput(nm,sm);
 
-let nm2, sm2=prompt("enter your Name and Symbol(X or O) Player 2: \n");
-const P2=playerInput(nm2 , sm2);
-
-let a=0,turn=0;
-while(a<9){//Game Logic
-    if(checkMatrix(board)==true){
-        console.log("player 1 has won");break;
+let nm2 =prompt("enter your Name Player 2: \n");
+if(P1.sm==="X"){
+    sm2="O";
+}
+else if(P1.sm==="O"){
+     sm2="X";
+}
+const P2=playerInput(nm2 ,sm2);
+cls.addEventListener("click", (e)=>{
+    let i= e.target.dataset.row;
+    let j= e.target.dataset.column;
+    if(mom.sameSpot(i,j)===true&&turn===1){
+        alert("PLEASE CHOOSE ANOTHER SPOT");turn=1;return ;
     }
-    if(checkMatrix(board)==false){
-        console.log("player 2 has won");break;
-    }
-    if(turn===0){
-        console.log("Player 1 turn: ");
-        let input=prompt("Enter the co-ords");
-        let [i,j]=input.split(" ");
-        mom.gameController(P1,i,j);
-        mom.dispBoard();turn=1;a++;continue;
+    else if(mom.sameSpot(i,j)===true&&turn===2){
+        alert("PLEASE CHOOSE ANOTHER SPOT");turn=2;return ;
     }
     if(turn===1){
-        console.log("Player 2 turn: ");
-        let input=prompt("Enter the co-ords");
-        let [i,j]=input.split(" ");
-        mom.gameController(P2,i,j);
-        turn=0;a++;
-        mom.dispBoard();continue;
+        mom.gameController(P1,i,j);turn=2;
     }
-        if(a===8){
-        console.log("It's a DRAW");break;
+    else if(turn==2){
+        mom.gameController(P2,i,j);turn=1;
     }
-}
+    mom.dispBoard();
+    const result = mom.checkMatrix();
+    if(result === true) {
+        alert(`${P1.nm} WON!!`);
+    } else if(result === false) {
+        alert(`${P2.nm2} WON!!`);
+    }
+})
+
+mom.createGameboard();
+
+// while(a<9){//Game Logic
+//     if(checkMatrix(board)==true){
+//         console.log("player 1 has won");break;
+//     }
+//     if(checkMatrix(board)==false){
+//         console.log("player 2 has won");break;
+//     }
+//     if(turn===0){
+//         console.log("Player 1 turn: ");
+//         let input=prompt("Enter the co-ords");
+//         let [i,j]=input.split(" ");
+//         mom.gameController(P1,i,j);
+//         mom.dispBoard();turn=1;a++;continue;
+//     }
+//     if(turn===1){
+//         console.log("Player 2 turn: ");
+//         let input=prompt("Enter the co-ords");
+//         let [i,j]=input.split(" ");
+//         mom.gameController(P2,i,j);
+//         turn=0;a++;
+//         mom.dispBoard();continue;
+//     }
+//         if(a===8){
+//         console.log("It's a DRAW");break;
+//     }
+// }
 
 // board.matrix.forEach(row => {
 //     disp(row.join(" "));
